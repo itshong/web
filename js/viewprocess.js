4dcda37dd81026f07538c50fdf80ba7c7852ce49
itshong <147592879+itshong@users.noreply.github.com>
2023-10-27 17:58:43 +0900

    20231027-6

---
+++ js/viewprocess.js
@@ -0,0 +1,16 @@
+
+
+// 탑버튼
+
+$(window).scroll(function(){
+    var wtt = $(this).scrollTop();
+    if (wtt > 500) {
+        $(".up_button").addClass("bt");
+    } else {
+        $(".up_button").removeClass("bt");
+    }
+});
+
+$(".up_button").click(function(){
+    $("html").animate({scrollTop:0},500);
+});
